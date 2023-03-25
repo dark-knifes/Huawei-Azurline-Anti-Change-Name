a91f63fb5305414149abc6fdb08687b4734619b2
dark-knifes <3450196385@qq.com>
2023-03-25 14:26:25 +0800

    edited

---
+++ patch_huawei2.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+# Download Azur Lane
+# Download Azur Lane
+download_azurlane () {
+    if [ ! -f "AzurLane.apk" ]; then
+   url="https://appdl6-drcn.dbankcdn.cn/dl/appdl/application/apk/7e/7e70bd9184a643ddaa4fc6cb9403817c/com.bilibili.blhx.huawei.2211101822.apk"
+    # 使用curl命令下载apk文件
+    curl -o blhx.apk  $url
+    fi
+}
+
+if [ ! -f "AzurLane.apk" ]; then
+    echo "Get Azur Lane apk"
+    download_azurlane
+    mv *.apk "AzurLane.apk"
+fi
+
+
+echo "Decompile Azur Lane apk"
+java -jar apktool.jar  -f d AzurLane.apk
+
+echo "Copy libs"
+cp -r lib/. AzurLane/libs/
+
+echo "Patching Azur Lane"
+oncreate=$(grep -n -m 1 'onCreate' AzurLane/smali/com/unity3d/player/UnityPlayerActivity.smali | sed  's/[0-9]*\:\(.*\)/\1/')
+sed -ir "s#\($oncreate\)#.method private static native init(Landroid/content/Context;)V\n.end method\n\n\1#" AzurLane/smali/com/unity3d/player/UnityPlayerActivity.smali
+sed -ir "s#\($oncreate\)#\1\n    const-string v0, \"Perseus\"\n\n\    invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\n\n    invoke-static {p0}, Lcom/unity3d/player/UnityPlayerActivity;->init(Landroid/content/Context;)V\n#" AzurLane/smali/com/unity3d/player/UnityPlayerActivity.smali
+sed -ir "s#\($oncreate\)#\1\n    const-string v0, \"Dev_Liu\"\n\n\    invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\n\n    invoke-static {p0}, Lcom/unity3d/player/UnityPlayerActivity;->init(Landroid/content/Context;)V\n#" AzurLane/smali/com/unity3d/player/UnityPlayerActivity.smali
+
+echo "Build Patched Azur Lane apk"
+java -jar apktool.jar  -f b AzurLane -o AzurLane.patched.apk
+
+echo "Set Github Release version"
+
+echo "PERSEUS_VERSION=$(echo Ship_Name)" >> $GITHUB_ENV
+
+mkdir -p build
+mv *.patched.apk ./build/
+find . -name "*.apk" -print
